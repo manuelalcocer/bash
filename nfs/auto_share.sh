c25cc612b4a4a5cdd4c1cb34747bb1d06d80176a
manuelalcocer <m.alcocer1978@gmail.com>
2016-01-13 08:12:02 +0100

    CheckMount function and vars

--- nfs/auto_share.sh
+++ nfs/auto_share.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-while IFS= read -r montaje; do
-    # 1: Servidor, 2: Recurso compartido, 3: Punto de montaje
-    { mountpoint -q $(cut -d: -f3 <<< ${montaje}) ;
-        } && { 
-                { showmount -e $(cut -d: -f1 <<< ${montaje}) \
-                    | grep -Ei "$(cut -d: -f2 <<< ${montaje})" ; 
-                } &> /dev/null || umount -l -f $(cut -d: -f3 <<< ${montaje}) ; 
-        } || {
-                { showmount -e $(cut -d: -f1 <<< ${montaje}) \
-                    | grep -Ei "$(cut -d: -f2 <<< ${montaje})" ;
-                } &> /dev/null && mount $(cut -d: -f3 <<< ${montaje}) ; 
-            }
-done < <(grep -Ei '^[^.*#]+.*\bnfs\b.*' /etc/fstab | awk '{print $1 ":" $2}')
+function CheckRemoteExport(){
+    # 1: Server, 2: Shared resource
+    showmount -e $(cut -d: -f1 <<< $@) \
+        | grep -Ei "$(cut -d: -f2 <<< $@)"
+}
+
+while IFS= read -r MountLine; do
+    # 3: Local mount point
+    MountPoint=$(cut -d: -f3 <<< ${MountLine})
+    { mountpoint -q ${MountPoint} ;
+        } && { CheckRemoteExport ${MountLine}  &> /dev/null \
+            || umount -l -f ${MountPoint} ;
+        } || { CheckRemoteExport ${MountLine} &> /dev/null \
+            && mount ${MountPoint} ; }
+done < <(grep -Ei '^[^.*#]+.*\bnfs\b.*' /etc/fstab \
+            | awk '{print $1 ":" $2}')
